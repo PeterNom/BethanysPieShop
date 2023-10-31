96f68bba841828a58bc25e329995ac5090d44065
Panagiotis Nomikos <peternomik@gmail.com>
2023-10-31 18:23:30 +0200

    Making the Site Interactive

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,5 +1,6 @@
 using BethanysPieShop.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +12,19 @@ builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp=> ShoppingCart.GetCar
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    }); ;
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
     options.UseSqlServer(
         builder.Configuration["ConnectionString:BethanysPieShopDbContextConnection"]);
 });
+
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
@@ -32,6 +39,8 @@ if(app.Environment.IsDevelopment())
 app.MapDefaultControllerRoute();
 
 app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/app/{*catchall}", "/App/Index");
 
 DbInitializer.Seed(app);
 
