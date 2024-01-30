a93b3e2fcd763d70424baaa4ed563c1868986ac5
Panagiotis Nomikos <102253139+PeterNom@users.noreply.github.com>
2024-01-30 14:11:56 +0200

    Creating the Repository.

---
+++ Models/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbcontext;
+
+        public CategoryRepository(BethanysPieShopDbContext bethanysPieShopDbcontext)
+        {
+            _bethanysPieShopDbcontext = bethanysPieShopDbcontext;
+        }
+
+        public IEnumerable<Category> AllCategories => _bethanysPieShopDbcontext.Categories.OrderBy(p => p.CategoryName);
+    }
+}
---
+++ Models/PieRepository.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbcontext;
+
+        public PieRepository(BethanysPieShopDbContext bethanysPieShopDbcontext)
+        {
+            _bethanysPieShopDbcontext = bethanysPieShopDbcontext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return _bethanysPieShopDbcontext.Pies.Include(c=>c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _bethanysPieShopDbcontext.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+
+        public Pie? GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
